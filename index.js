d0e0595d90a204102141d8ab6e3fbabf5e33ca72
Nathanael Gonthier <gontthier.pro@gmail.com>
2022-05-19 19:42:36 +0400

    3_JSX

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 
 function Header(){
-    return (<h1>La maison jungle !</h1>)    
+    const titre = "la maison jungle !";
+    return (<h1>{titre.toUpperCase()}</h1>)    
 }
 
-ReactDOM.render(<Header/>,document.getElementById("root"))
+function Description(){
+    const text = "Ici achetez toutes les plantes dont vous avez toujours rêvé";
+    const emojis = "😋😊😉"
+    return (<p>{text.slice(0,11)+emojis}</p>)
+}
+
+function Cart(){
+    const monstera = 8;
+    const lierre = 10;
+    const fleur = 15;
+    var total = monstera + lierre + fleur;
+
+    return (<div>
+        <ul>
+            <li>monstera : {monstera} €</li>
+            <li>lierre  : {lierre} €</li>
+            <li>bouquet de fleur : {fleur} €</li>
+        </ul>
+        total : {total} €
+    </div>)
+}
+
+function Banner(){
+    return (<div>
+        <Header/>
+        <Cart/>
+        <Description/>
+    </div>)
+}
+
+ReactDOM.render(<Banner/>,document.getElementById("root"))
